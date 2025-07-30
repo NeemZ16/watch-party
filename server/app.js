acec3398029bd3ef82f6e81c902658989753ef55
NeemZ16 <110858265+NeemZ16@users.noreply.github.com>
2025-07-29 23:35:15 -0400

    added user router, controller, updated to ESM from commonJS

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send("hello world - alvi");
 })
 
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
