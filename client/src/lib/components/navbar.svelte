fbc42e50d07041cba5553742a26f2bac6f9978d6
NeemZ16 <110858265+NeemZ16@users.noreply.github.com>
2025-11-01 20:20:45 -0400

    added base navbar

---
+++ client/src/lib/components/navbar.svelte
@@ -0,0 +1,18 @@
+<script>
+    import logo from '$lib/assets/logo.svg';
+</script>
+
+<div id="navbar">
+    <img src={logo} alt="watch party!">
+</div>
+
+<style>
+    #navbar {
+        background-color: var(--neutral);
+        padding: 0.5rem var(--page-padding);
+    }
+    
+    img {
+        width: 200px;
+    }
+</style>
--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script>
+	import '../app.css'
 	import favicon from '$lib/assets/favicon.svg';
+	import { title } from 'process';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>watch party!</title>
 </svelte:head>
 
 {@render children?.()}
